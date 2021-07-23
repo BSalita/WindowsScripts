4039b3a34c233f3984cf4b1bc8f2b2ac1d726c3a
BSalita <BSalita@yahoo.com>
2021-07-23 11:46:39 +0200

    Delete kali-plasma.bat

--- kali-plasma.bat
+++
@@ -1,1 +0,0 @@
-C:\Users\bsali\AppData\Local\Microsoft\WindowsApps\gwsl.exe --r --wsl_machine="kali-linux" --wsl_cmd="startplasma-x11" --w_mode="Single" --clip_enabled="Default" --gtk_scale="Default" --qt_scale="Default" --append="" --theme="follow" --root="false" --dbus="false"
