0f93ea6cb42475d78679d730463fc1d314f94c56
BSalita <BSalita@yahoo.com>
2021-07-23 11:44:09 +0200

    Delete ubuntu-plasma.bat

--- ubuntu-plasma.bat
+++
@@ -1,1 +0,0 @@
-C:\Users\bsali\AppData\Local\Microsoft\WindowsApps\gwsl.exe --r --wsl_machine="Ubuntu" --wsl_cmd="startplasma-11" --w_mode="Fullscreen" --clip_enabled="Default" --gtk_scale="Default" --qt_scale="Default" --append="" --theme="follow" --root="false" --dbus="false"
