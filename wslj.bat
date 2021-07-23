03f632a8d940ca6321902efb2ffea059b8167e10
BSalita <BSalita@yahoo.com>
2021-07-23 11:43:06 +0200

    Delete wslj.bat

--- wslj.bat
+++
@@ -1,2 +0,0 @@
-@rem wsl -d Ubuntu-20.04 -u robert bash -c -i "cd /mnt/c/sw/bridge/TheCommonGame && conda activate fastai2 && jupyter lab" 
-wsl -d Ubuntu-20.04 -u robert bash -c -i "cd /mnt/c/sw/bridge/TheCommonGame && jupyter lab" 
