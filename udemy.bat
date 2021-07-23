4bae4f9b12fe71eb6669adb63c07309597e4ad33
BSalita <BSalita@yahoo.com>
2021-07-23 11:43:50 +0200

    Delete udemy.bat

--- udemy.bat
+++
@@ -1,5 +0,0 @@
-rem install latest version of udemy-dll: python.exe -m pip install --upgrade https://github.com/nishad/udemy-dl/zipball/master
-rem ignore any "InsecurePlatformWarning"
-rem if download errors out, manually restart by using --lecture-start followed by the next lecture's number: e.g. --lecture-start 4
-cd c:\sw\udemy
-bash -c "udemy-dl -u bsalita@yahoo.com -p ud7665 %1"
