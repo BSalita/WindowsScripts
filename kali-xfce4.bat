1e3b1e0be7c32ab8a9d5194b854a959326eb35f4
BSalita <BSalita@yahoo.com>
2021-07-23 11:46:30 +0200

    Delete kali-xfce4.bat

--- kali-xfce4.bat
+++
@@ -1,1 +0,0 @@
-C:\Users\bsali\AppData\Local\Microsoft\WindowsApps\gwsl.exe --r --wsl_machine="kali-linux" --wsl_cmd="xfce4-session" --w_mode="Single" --clip_enabled="Default" --gtk_scale="Default" --qt_scale="Default" --append="" --theme="follow" --root="false" --dbus="false"
