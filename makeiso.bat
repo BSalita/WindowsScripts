afda79dbe6d3fd41a1f3bd016104fddab3b75be6
BSalita <BSalita@yahoo.com>
2021-07-23 11:45:29 +0200

    Delete makeiso.bat

--- makeiso.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-if exist makeiso.err erase/q makeiso.err
-if exist makeiso.log erase/q makeiso.log
-set DIRS=%1
-if not defined DIRS set DIRS=*
-set MakeIsoBatch=yes
-for /d %%i in (%DIRS%) do @call "MAKEISO1.BAT" "%%~i" %2
-set MakeIsoBatch=
-if exist makeiso.err erase/q makeiso.err
