bb48f8ff25d076f419f7d7fae539f495bab13fb5
BSalita <BSalita@yahoo.com>
2021-07-23 11:34:46 +0200

    Delete InstallWebdriversBypyderman.bat

--- InstallWebdriversBypyderman.bat
+++
@@ -1,4 +0,0 @@
-pip install pyderman
-py -c "import pyderman as driver; path = driver.install(browser=driver.chrome)"
-py -c "import pyderman as driver; path = driver.install(browser=driver.edge)"
-py -c "import pyderman as driver; path = driver.install(browser=driver.firefox)"
